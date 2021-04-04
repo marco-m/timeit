f32cceab134359a50d11de4eccacf8f7f769dc5e
Marco Molteni <marco.molteni@mailbox.org>
2021-04-04 20:51:09 +0200

    sleepit: add signal handling
    This should make it easier and more evident to test that timeit behaves correctly on reception of SIGINT.

--- cmd/sleepit/main.go
+++ cmd/sleepit/main.go
@@ -7,28 +7,54 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/signal"
 	"time"
 )
 
-const usage = "usage: sleepit <duration>"
+const usage = "usage: sleepit <sleep-duration> <cleanup-duration>"
 
 func main() {
 	os.Exit(run(os.Args, os.Stderr))
 }
 
 func run(args []string, out io.Writer) int {
-	if len(args) != 2 {
+	if len(args) != 3 {
 		fmt.Fprintln(out, usage)
 		return 2
 	}
 
-	duration, err := time.ParseDuration(args[1])
+	sleepDuration, err := time.ParseDuration(args[1])
 	if err != nil {
-		fmt.Fprintln(out, err)
+		fmt.Fprintln(out, "parsing sleep-duration:", err)
 		fmt.Fprintln(out, usage)
 		return 2
 	}
 
-	time.Sleep(duration)
+	cleanupDuration, err := time.ParseDuration(args[2])
+	if err != nil {
+		fmt.Fprintln(out, "parsing cleanup-duration:", err)
+		fmt.Fprintln(out, usage)
+		return 2
+	}
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt) // Ctrl-C -> SIGINT
+	fmt.Printf("sleepit: ready:\n")
+	fmt.Printf("sleepit: do one of the following:\n")
+	fmt.Printf("- From this shell:    type: CTRL-C\n")
+	fmt.Printf("- From another shell: type: kill -INT %d\n", os.Getpid())
+
+	fmt.Printf("sleepit: working...\n")
+	select {
+	case <-time.After(sleepDuration):
+		fmt.Printf("sleepit: work done\n")
+	case sig := <-sigCh:
+		fmt.Printf("sleepit: got signal: %v\n", sig)
+		fmt.Printf("sleepit: cleaning up, please wait\n")
+		time.Sleep(cleanupDuration)
+		fmt.Printf("sleepit: cleanup done\n") // <== NOTE THIS LINE
+		return 3
+	}
+
 	return 0
 }
