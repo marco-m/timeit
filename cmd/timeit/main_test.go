61802cd9144c76b09f313e55bd781c0654ec4a76
Marco Molteni <marco.molteni@mailbox.org>
2021-04-15 20:41:58 +0200

    timeit: tests: extract unix-specific tests to separate file

--- cmd/timeit/main_test.go
+++ cmd/timeit/main_test.go
@@ -5,13 +5,9 @@ package main
 
 import (
 	"bytes"
-	"errors"
-	"os/exec"
 	"path/filepath"
 	"strings"
-	"syscall"
 	"testing"
-	"time"
 )
 
 var (
@@ -109,77 +105,6 @@ func parseOutput(out string) (timeitErrLine string, containsTimeitResults bool) 
 		}
 	}
 	return errLine, containsResults
-}
-
-func TestSignalSentToProcessGroup(t *testing.T) {
-	var out bytes.Buffer
-	sut := execCommand(TIMEIT, SLEEPIT, "2s", "10ms")
-	sut.Stdout = &out
-	sut.Stderr = &out
-
-	// Create a new process group, by setting the process group ID of the child to the
-	// child PID.
-	// By default, the child would inherit the process group of the parent, but we want
-	// to avoid this, to protect the parent (the test process) from the signal that this
-	// test will send. More info in the comments below for syscall.Kill().
-	sut.SysProcAttr = &syscall.SysProcAttr{Setpgid: true, Pgid: 0}
-
-	if err := sut.Start(); err != nil {
-		t.Fatalf("starting the timeit process: %v", err)
-	}
-
-	// After the child is started, we want to avoid a race condition where we send it a
-	// signal before it had time to setup its own signal handlers.
-	// Sleeping is way too flaky, instead we parse the child output until we get a line
-	// that we know is printed after the signal handlers are installed...
-	ready := false
-	timeout := time.Duration(time.Second)
-	start := time.Now()
-	for time.Since(start) < timeout {
-		if strings.Contains(out.String(), "sleepit: ready") {
-			ready = true
-			break
-		}
-		time.Sleep(10 * time.Millisecond)
-	}
-	if !ready {
-		t.Fatalf("sleepit not ready after %v", timeout)
-	}
-
-	// When we have a running program in a shell and type CTRL-C, the tty driver will
-	// send a SIGINT signal to all the processes in the foreground process group
-	// (see https://en.wikipedia.org/wiki/Process_group).
-	//
-	// Here we want to emulate this behavior: send SIGINT to the process group of the
-	// test executable. Although Go for some reasons doesn't wrap the killpg(2) system
-	// call, what works is using syscall.Kill(-PID, SIGINT), where the negative PID means
-	// the corresponding process group. Note that this negative PID works only as long
-	// as the caller of the kill(2) system call has a different PID, which is the case
-	// for this test.
-	if err := syscall.Kill(-sut.Process.Pid, syscall.SIGINT); err != nil {
-		t.Fatalf("sending INT signal to the process group: %v", err)
-	}
-
-	err := sut.Wait()
-
-	var wantErr *exec.ExitError
-	const wantExitStatus = 3 // sleepit returns 3 if it receives SIGINT
-	if errors.As(err, &wantErr) {
-		if wantErr.ExitCode() != wantExitStatus {
-			t.Errorf("waiting for the timeit process: got exit status %v; want %d",
-				wantErr.ExitCode(), wantExitStatus)
-			t.Errorf("exited normally (that is: not terminated by a signal): %v",
-				wantErr.Exited())
-			t.Errorf("Process state: %q", wantErr.String())
-		}
-	} else {
-		t.Errorf("waiting for the timeit process: got %v (%T); want (%T)", err, err, wantErr)
-	}
-
-	wantMsg := "sleepit: cleanup done"
-	if !strings.Contains(out.String(), wantMsg) {
-		t.Errorf("output: %q does not contain %q", out.String(), wantMsg)
-	}
 }
 
 func TestShowVersion(t *testing.T) {
