ec38995937341163ad52cf1eb64a826b8843f472
Marco Molteni <marco.molteni@mailbox.org>
2023-01-16 15:18:48 +0100

    start using the super-powerful github.com/rogpeppe/go-internal
    Made understandable by https://bitfieldconsulting.com/golang/test-scripts

---
+++ cmd/understand-script/understand-script_test.go
@@ -0,0 +1,23 @@
+package main_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/marco-m/timeit/cmd/understand-script"
+
+	"github.com/rogpeppe/go-internal/testscript"
+)
+
+func TestMain(m *testing.M) {
+	os.Exit(testscript.RunMain(m, map[string]func() int{
+		"understand-script": main.Main,
+	}))
+}
+
+// NOTE Since understand-script calls os.Exit directly, we miss coverage information :-(
+func TestWithScript(t *testing.T) {
+	testscript.Run(t, testscript.Params{
+		Dir: "testdata",
+	})
+}
--- go.mod
+++ go.mod
@@ -6,10 +6,12 @@ require (
 	github.com/fatih/color v1.13.0
 	github.com/marco-m/taschino v0.0.0-20200608160751-9822f843b063
 	github.com/mattn/go-isatty v0.0.17
+	github.com/rogpeppe/go-internal v1.9.1-0.20230113211820-9957a5216c11
 )
 
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/pkg/diff v0.0.0-20210226163009-20ebb0f2a09e // indirect
 	golang.org/x/mod v0.7.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 )
